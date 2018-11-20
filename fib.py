ad2bbc8d8f2881474a60ea603bd75c4c37aed109
Jakub <witos1171@gmail.com>
2018-11-20 23:11:45 +0100

    DodanieEnterow

--- fib.py
+++ fib.py
@@ -2,8 +2,11 @@ from functools import lru_cache
 
 @lru_cache(maxsize = 1000)
 def fibonacci(n):
-    if n ==1 : return 1
-    elif n==2 : return 1
-    elif n>2 : return fibonacci(n-1) + fibonacci(n-2)
-for n in range(1,1000):
+    if n ==1 :
+        return 1
+    elif n==2 :
+        return 1
+    elif n>2 :
+        return fibonacci(n-1) + fibonacci(n-2)
+for n in range(1,100):
     print(n,":",fibonacci(n))
