1a2a4cf62829b7ad9c471883f2aa0e926187fa68
Jakub <witos1171@gmail.com>
2018-11-21 16:32:29 +0100

    Dobry Uklad entery

--- fib.py
+++ fib.py
@@ -1,12 +1,15 @@
 from functools import lru_cache
 
-@lru_cache(maxsize = 1000)
+
+@lru_cache(maxsize=1000)
 def fibonacci(n):
-    if n ==1 :
+    if n == 1:
         return 1
-    elif n==2 :
+    elif n == 2:
         return 1
-    elif n>2 :
-        return fibonacci(n-1) + fibonacci(n-2)
-for n in range(1,100):
-    print(n,":",fibonacci(n))
+    elif n > 2:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+for n in range(1, 100):
+    print(n, ":", fibonacci(n))
